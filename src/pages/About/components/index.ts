458112a9df65648ac3ebaa5ba8c9e61a34ada18b
mz0018 <martinezhanzmenzi@gmail.com>
2026-03-05 22:10:56 +0800

    corrupted files?

--- src/pages/About/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { Overview } from './Overview';
-export { Lens } from './Lens';
-export { MunicipalProfile } from './MunicipalProfile';
-export { BusinessCore } from './BusinessCore';
-export { PeopleAsPower } from './PeopleAsPower';
--- src/pages/Home/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { Hero } from './Hero';
-export { Stats } from './Stats';
-export { WhyInvest } from './WhyInvest';
-export { PriorityAreas } from './PriorityAreas';
-export { IncentivesPreview } from './IncentivesPreview';
-export { CTA } from './CTA';
